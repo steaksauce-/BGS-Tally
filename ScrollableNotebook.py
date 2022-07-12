e62000e126104d61d007f216c08c6ebe2f20b49f
aussig <austin@opensceneryx.com>
2022-07-12 18:45:54 +0100

    Update ScrollableNotebook to latest

--- ScrollableNotebook.py
+++ ScrollableNotebook.py
@@ -19,7 +19,7 @@ class ScrollableNotebook(ttk.Frame):
         self.menuSpace=30
         if tabmenu==True:
             self.menuSpace=50
-            bottomTab = ttk.Label(slideFrame, text=" \u2630 ")
+            bottomTab = ttk.Label(slideFrame, text="\u2630")
             bottomTab.bind("<ButtonPress-1>",self._bottomMenu)
             bottomTab.pack(side=RIGHT)
         leftArrow = ttk.Label(slideFrame, text=" \u276E")
@@ -31,7 +31,7 @@ class ScrollableNotebook(ttk.Frame):
         rightArrow.bind("<ButtonRelease-1>",self._slideStop)
         rightArrow.pack(side=RIGHT)
         self.notebookContent.bind("<Configure>", self._resetSlide)
-        self.timer = None
+        self.contentsManaged = []
 
     def _wheelscroll(self, event):
         if event.delta > 0:
@@ -90,10 +90,14 @@ class ScrollableNotebook(ttk.Frame):
         else:
             self.notebookContent.add(frame, text="")
         self.notebookTab.add(ttk.Frame(self.notebookTab),**kwargs)
+        self.contentsManaged.append(frame)
 
     def forget(self,tab_id):
+        index = self.notebookTab.index(tab_id)
         self.notebookContent.forget(self.__ContentTabID(tab_id))
         self.notebookTab.forget(tab_id)
+        self.contentsManaged[index].destroy()
+        self.contentsManaged.pop(index)
 
     def hide(self,tab_id):
         self.notebookContent.hide(self.__ContentTabID(tab_id))
