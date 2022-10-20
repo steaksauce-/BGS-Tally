d159a955344370bfbc90bd47e38369929dcc6d8b
aussig <austin@opensceneryx.com>
2022-10-20 19:06:15 +0100

    More WIP on target logging - Inara CMDR data fetch and caching.

--- bgstally/targetlog.py
+++ bgstally/targetlog.py
@@ -1,14 +1,18 @@
 import json
-import re
 import os.path
-from typing import Dict
+import re
 from datetime import datetime, timedelta
+from typing import Dict
+
+import requests
 
 from bgstally.constants import DATETIME_FORMAT_JOURNAL
 from bgstally.debug import Debug
 
 FILENAME = "targetlog.json"
 TIME_TARGET_LOG_EXPIRY_D = 30
+URL_INARA_API = "https://inara.cz/inapi/v1/"
+DATETIME_FORMAT_INARA = "%Y-%m-%dT%H:%M:%SZ"
 
 
 class TargetLog:
@@ -20,6 +24,7 @@ class TargetLog:
     def __init__(self, bgstally):
         self.bgstally = bgstally
         self.targetlog = []
+        self.cmdr_cache = {}
         self.load()
 
 
@@ -54,18 +59,72 @@ class TargetLog:
         """
         # { "timestamp":"2022-10-09T06:49:06Z", "event":"ShipTargeted", "TargetLocked":true, "Ship":"cutter", "Ship_Localised":"Imperial Cutter", "ScanStage":3, "PilotName":"$cmdr_decorate:#name=[Name];", "PilotName_Localised":"[CMDR Name]", "PilotRank":"Elite", "SquadronID":"TSPA", "ShieldHealth":100.000000, "HullHealth":100.000000, "LegalStatus":"Clean" }
         if not 'ScanStage' in journal_entry or journal_entry['ScanStage'] < 3: return
-        if not 'PilotName' in journal_entry or not journal_entry['PilotName'].startswith("$cmdr_decorate:#name"): return
+        if not 'PilotName' in journal_entry: return
 
         cmdr_match = self.cmdr_name_pattern.match(journal_entry['PilotName'])
         if not cmdr_match: return
+
         cmdr_name = cmdr_match.group(1)
 
-        self.targetlog.append({'TargetName': cmdr_name,
-                                'System': system,
-                                'SquadronID': journal_entry['SquadronID'] if 'SquadronID' in journal_entry else "----",
-                                'Ship': journal_entry['Ship_Localised'],
-                                'LegalStatus': journal_entry['LegalStatus'],
-                                'Timestamp': journal_entry['timestamp']})
+        cmdr_data = {'TargetName': cmdr_name,
+                    'System': system,
+                    'SquadronID': journal_entry['SquadronID'] if 'SquadronID' in journal_entry else "----",
+                    'Ship': journal_entry['Ship'],
+                    'LegalStatus': journal_entry['LegalStatus'],
+                    'Timestamp': journal_entry['timestamp']}
+
+        cmdr_data = self.fetch_cmdr_info(cmdr_name, cmdr_data)
+        self.targetlog.append(cmdr_data)
+
+        Debug.logger.info(cmdr_data)
+
+
+
+    def fetch_cmdr_info(self, cmdr_name:str, cmdr_data:Dict):
+        """
+        Fetch additional CMDR data from Inara and enhance the cmdr_data Dict with it
+        """
+        if cmdr_name in self.cmdr_cache: return self.cmdr_cache[cmdr_name]
+
+        payload = {
+            'header': {
+                'appName': self.bgstally.plugin_name,
+                'appVersion': self.bgstally.version,
+                'isBeingDeveloped': "true",
+                'APIkey': self.bgstally.config.apikey_inara()
+            },
+            'events': [
+                {
+                    'eventName': "getCommanderProfile",
+                    'eventTimestamp': datetime.utcnow().strftime(DATETIME_FORMAT_INARA),
+                    'eventData': {
+                        'searchName': cmdr_name
+                    }
+                }
+            ]
+        }
+
+        try:
+            response = requests.post(URL_INARA_API, json=payload, timeout=10)
+            response.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            Debug.logger.error(f"Unable to fetch CMDR Profile from Inara", exc_info=e)
+            return cmdr_data
+
+        data = response.json()
+        if not 'events' in data or len(data['events']) == 0 or not 'eventData' in data['events'][0]: return cmdr_data
+
+        event_data = data['events'][0]['eventData']
+
+        if 'commanderRanksPilot' in event_data:
+            cmdr_data['ranks'] = event_data['commanderRanksPilot']
+        if 'commanderSquadron' in event_data:
+            cmdr_data['squadron'] = event_data['commanderSquadron']
+        if 'inaraURL' in event_data:
+            cmdr_data['inaraURL'] = event_data['inaraURL']
+
+        self.cmdr_cache[cmdr_name] = cmdr_data
+        return cmdr_data
 
 
     def _expire_old_targets(self):
