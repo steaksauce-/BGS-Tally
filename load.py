eea88312ff9a932626965b0ead5d649abdfe6a1e
aussig <austin@opensceneryx.com>
2022-12-05 17:00:05 +0000

    Update version number for release

--- load.py
+++ load.py
@@ -3,7 +3,7 @@ from os import path
 from bgstally.bgstally import BGSTally
 from bgstally.constants import UpdateUIPolicy
 
-PLUGIN_VERSION = "2.0.2"
+PLUGIN_VERSION = "2.1.0"
 
 # Initialise the main plugin class
 this:BGSTally = BGSTally(path.basename(path.dirname(__file__)), PLUGIN_VERSION)
